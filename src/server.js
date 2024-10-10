421b512f9d9d1c6986cf7caf0acbd80bc0703471
Raul Elizalde <raulei2323@gmail.com>
2024-10-09 20:36:11 -0600

    server ready??

---
+++ src/server.js
@@ -0,0 +1,18 @@
+
+const express = require('express')
+
+
+const app = express()
+
+app.use(express.json())
+
+
+
+app.get("/", (request, response) => {
+    response.json({
+        message: "Api accessGo"
+    })
+})
+
+
+module.exports = app
