cf0e4cabad3238ce6a471b7b41336f41303e5c79
GloDelMar <160694701+GloDelMar@users.noreply.github.com>
2025-01-13 18:51:33 -0600

    Update visits.model.js
    cambios a modelo de visitas

--- src/models/visits.model.js
+++ src/models/visits.model.js
@@ -4,7 +4,17 @@ const visitSchema = new mongoose.Schema({
     page: { type: String, required: true },
     companyId: { type: String, required: true },
     visits: { type: Number, default: 0 },
-    visitDates: [{ date: { type: Date, default: Date.now } }] // Almacena las fechas de las visitas
+    visitDates: [{ date: { type: Date, default: Date.now } }],
+    ipAddresses: [
+        {
+            ip: String,
+            lastVisit: { type: Date, default: Date.now },
+        },
+    ],
+    recentVisitors: [ 
+        {
+             ip: String, 
+             lastVisit: { type: Date, default: Date.now }, } ]
 });
 
 const Visit = mongoose.model('Visit', visitSchema);
